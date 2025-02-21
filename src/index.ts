80c3adb6515d5d324243c94302c681a661f6cfbf
Angel Chacon <angel1998chacon@gmail.com>
2025-02-21 15:19:49 -0600

    Añadiendo typescript

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 app.get('/', (req, res) => {
-    res.send('Hello World! in express 33');
+    res.send('Hello World! in express /TypeScript');
 });
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
